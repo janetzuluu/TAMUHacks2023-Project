38aabc05afa2d9d7df0ea0251a37382fd00101b5
janet <jzulu01@jaguar.tamu.edu>
2023-01-28 15:27:11 -0600

    test3
    test3

--- lane.py
+++ lane.py
@@ -1,6 +1,6 @@
 import cv2 
 import numpy as np
-image=cv2.imread('C:/Users/janet/Downloads/IMG_2713.jpg')
+image=cv2.imread('IMG_2713.jpg')
 laneImage=np.copy(image)
 cv2.imshow("result",image)
 cv2.waitKey(0)
