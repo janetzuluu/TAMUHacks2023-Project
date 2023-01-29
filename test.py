31199a118b4519f0ba1a02a58de62ba3e6d57c96
janet <jzulu01@jaguar.tamu.edu>
2023-01-29 10:24:17 -0600

    last commit with comments
    Last commit with better comments

--- test.py
+++ test.py
@@ -6,24 +6,21 @@ img = cv2.imread('C:/Users/janet/Downloads/IMG_2720.jpg')
 
 # Convert the image to grayscale 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# Apply gussian blur to reduce noise and make analyzing easier
 blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
-# detect edges in the picture
 edges = cv2.Canny(blur, 50, 150)
 
-# Region of interest of the shape we actually want to look at
+# Area of interest of the shape we actually want to look at
 height, width = edges.shape
 mask = np.zeros_like(edges)
 polygon = np.array([[(0, height), (width/2, height/2), (width, height)]], np.int32)
 cv2.fillPoly(mask, polygon, 255)
 masked_edges = cv2.bitwise_and(edges, mask)
 
-# Apply HoughLinesP to detect lines
+# Apply HoughLines algo to detect lines on the picture with the masked edges
 lines = cv2.HoughLinesP(masked_edges, 1, np.pi/180, 50, maxLineGap=50)
 
-# Split the lines into left and right lanes
+# Split the lines into left and right lanes into arrays
 left_lines = []
 right_lines = []
 for line in lines:
@@ -32,16 +29,13 @@ for line in lines:
         left_lines.append(line)
     elif x1 > width/2 and x2 > width/2:
         right_lines.append(line)
-
 # Average the lines in each lane to get a single lane line
 left_avg = np.mean(left_lines, axis=0).flatten().astype(int)
 right_avg = np.mean(right_lines, axis=0).flatten().astype(int)
-
-# Draw the lane lines on the image
 cv2.line(img, (left_avg[0], left_avg[1]), (left_avg[2], left_avg[3]), (0, 255, 0), 2)
 cv2.line(img, (right_avg[0], right_avg[1]), (right_avg[2], right_avg[3]), (0, 255, 0), 2)
 
-# Show the image with lane lines
+#lane lines and show for a specificied time then close it
 cv2.imshow("Lanes", img)
 cv2.waitKey(7000)
 cv2.destroyAllWindows()
